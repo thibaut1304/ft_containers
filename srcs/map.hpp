ce04b5601e38350e5513c215f1a050e42a80e592
thibaut1304 <thibaut.husser@free.fr>
2022-07-15 22:06:48 +0200

    ajout def map manque juste value_compare

--- srcs/map.hpp
+++ srcs/map.hpp
@@ -6,7 +6,7 @@
 /*   By: thhusser <thhusser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 21:04:00 by thhusser          #+#    #+#             */
-/*   Updated: 2022/07/15 21:32:36 by thhusser         ###   ########.fr       */
+/*   Updated: 2022/07/15 22:02:37 by thhusser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 # include "reverse_iterator.hpp"
 
+// # include <cstddef> // ptrdiff -->  dans iterator_traits
 
 
 namespace ft {
@@ -27,16 +28,22 @@ namespace ft {
 			class map {
 				public:
 
-					typedef	key 										key_type;
-					typedef	T											mapped_type;
-					typedef	pair<const key_type,mapped_type>			value_type;     //coder PAIR !
-					typedef	Compare										key_coompare;
+					typedef	key 														key_type;
+					typedef	T															mapped_type;
+					typedef	pair<const key_type,mapped_type>							value_type;     //coder PAIR !
+					typedef	Compare														key_coompare;
 					// typedef --> regarder value_compare nouv class ?
-					typedef Alloc										allocator_type;
-					typedef typename allocator_type::reference			reference;
-					typedef typename allocator_tyoe::const_reference	const_reference;
-					typedef typename allocator_type::pointer			pointer;
-					typedef typename allocator_type::const_pointer		const_pointer;
+					typedef Alloc														allocator_type;
+					typedef typename allocator_type::reference							reference;
+					typedef typename allocator_type::const_reference					const_reference;
+					typedef typename allocator_type::pointer							pointer;
+					typedef typename allocator_type::const_pointer						const_pointer;
+					typedef typename ft::bidirectional_iterator<value_type>				iterator;		// coder bidirectional_iterator
+					typedef typename ft::bidirectional_iterator<value_type const>		const_iterator; // coder bidirectional_iterator
+					typedef typename ft::reverse_iterator<value_type>					reverse_iterator;
+					typedef typename ft::reverse_iterator<value_type const>				const_reverse_iterator;
+					typedef typename ft::iterator_traits<iterator>::difference_type		difference_type;
+					typedef typename std::size_t										size_type;
 
 
 
