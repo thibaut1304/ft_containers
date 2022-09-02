29233b520ee30c669236acfe315748ce4e5ef11c
Thibaut1304 <thibaut.husser@free.fr>
2022-09-02 15:30:18 +0200

    arbre binaire fonctionnel dans cet etat avec parend et _end

--- tests/test_BST.cpp
+++ tests/test_BST.cpp
@@ -9,15 +9,15 @@ int main () {
 	// std::cout << sizeof(int) << " "
 	tree<int, std::string> arbre;
 	//insert key et T
-	arbre.insert(40, "hi");
+	arbre.insert(42, "hi");
 	arbre.insert(50, "hi");
-	arbre.insert(14, "hi");
-	arbre.insert(16, "hi");
-	arbre.insert(5, "hello");
-	arbre.insert(3, "3");
-	arbre.insert(7, "b");
-	arbre.insert(55, "hi");
-	arbre.insert(45, "hi");
+	arbre.insert(35, "hi");
+	arbre.insert(21, "hi");
+	arbre.insert(38, "hello");
+	arbre.insert(45, "3");
+	arbre.insert(55, "b");
+	// arbre.insert(55, "hi");
+	// arbre.insert(45, "hi");
 
 	// arbre.insert(9, "hello");
 	// arbre.insert(5, "el padre");
@@ -33,12 +33,12 @@ int main () {
 	std::cout << "Taille de  l'arbre : " << arbre.size() << std::endl;
 
 	// std::cout  << "Supression 25 !" << std::endl;
-	arbre.toDelete(14);
+	// arbre.toDelete(14);
 	// arbre.toDelete(26);
 	// std::cout  << "Affichage apres suprission 25 avec 2 enfants !" << std::endl;
-	arbre.infixe();
-	std::cout << "Taille de  l'arbre : " << arbre.size() << std::endl;
-	std::cout << "\n\n------------------------------------\n\n";
+	// arbre.infixe();
+	// std::cout << "Taille de  l'arbre : " << arbre.size() << std::endl;
+	// std::cout << "\n\n------------------------------------\n\n";
 
 	// tree<std::string, std::string> three;
 
